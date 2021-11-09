47aa755f4a1876666edf85b5e13698133e02fc2c
bgravanda <bgravanda@student.bridgew.edu>
2021-11-09 10:43:34 -0500

    Initial commit

---
+++ src/main/java/bsu/comp152/windowwebdata/DataHandler.java
@@ -0,0 +1,4 @@
+package bsu.comp152.windowwebdata;
+
+public class DataHandler {
+}
---
+++ src/main/java/bsu/comp152/windowwebdata/UniversityApplication.java
@@ -0,0 +1,29 @@
+package bsu.comp152.windowwebdata;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class UniversityApplication extends Application {
+    @Override
+    public void start(Stage stage) {
+        FXMLLoader fxmlLoader = new FXMLLoader(UniversityApplication.class.getResource("Main.fxml"));
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load(), 600, 400);
+        }
+        catch (IOException e){
+            System.out.println("Error loading FXML or exception in the controller class");
+        }
+        stage.setTitle("Showing University Data");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
